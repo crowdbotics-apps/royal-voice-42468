58f3d91c67b79761471b384b6219da7697bb2fad
Crowdbotics <team@crowdbotics.com>
2023-07-27 19:43:49 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "royal-voice-42468",
+  url: "https://royal-voice-42468.botics.co",
+  api: "https://royal-voice-42468.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
